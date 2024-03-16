bea4146756068bfe37e29011440e78defd283b6e
cmp0xff <cmp0xff@gmail.com>
2024-03-16 19:19:59 +0100

    doc(pendulum): #14 placeholders

---
+++ docs/tutorials/pendulum.py
@@ -0,0 +1,22 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.16.1
+#   kernelspec:
+#     display_name: .venv
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# # Pendulum
+#
+# In this tutorial, we demo how to generate data of a pendulum.
+
+# %% [markdown]
+# End of Notebook
